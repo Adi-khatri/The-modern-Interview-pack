2c9193aea97b067051426f925ac811d4d8e92eed
Rishit Saraf <54949307+rishitsaraf@users.noreply.github.com>
2021-06-27 01:25:45 +0530

    Add files via upload

--- notes/js/app.js
+++ notes/js/app.js
@@ -30,7 +30,7 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+            <div style="width: 18rem;border-radius:15px;" class="noteCard my-2 mx-2 card ">
                     <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
                         <p class="card-text"> ${element}</p>
